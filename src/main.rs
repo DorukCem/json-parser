37fc9d3cbf1b62b2483117e1501d45a6af51c134
Doruk <doruk.cem@gmail.com>
2024-11-21 18:28:21 +0300

    cannot parse simple key-values

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
 
+#[derive(Debug, PartialEq, Eq)]
 enum JsonToken {
     Object(JsonObject),
+    String(String),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct JsonObject {
-    keys: HashMap<String, JsonToken>,
+    keys: Option<HashMap<String, JsonToken>>,
 }
 
 #[derive(Debug)]
-struct SomeError {}
+struct SomeError {
+    msg: String,
+}
 
 struct Parser {
     index: usize,
     chars: Vec<char>,
+}
+
+enum ParseState {
+    Key,
+    Value,
 }
 
 impl Parser {
@@ -31,17 +41,96 @@ impl Parser {
     }
 
     fn parse_object(&mut self) -> Result<JsonObject, SomeError> {
-        if Some(&'{') != self.pop() {
-            return Err(SomeError {});
+        let first = self.pop();
+        if Some(&'{') != first {
+            return Err(SomeError {
+                msg: format!("Expected object to start with: {{ instead got: {:?}", {
+                    first.unwrap()
+                }),
+            });
+        }
+        self.skip_whitespace();
+
+        let mut parse_state = ParseState::Key;
+        let mut current_key = None;
+        let mut keys: HashMap<String, JsonToken> = HashMap::new();
+        let mut expect_one_more_kv_pair = false;
+
+        // Read key : value pairs
+        while !self.is_done() {
+            self.skip_whitespace();
+            match (&parse_state, self.peek()) {
+                (ParseState::Key, Some(&'}')) => break,
+                (ParseState::Key, Some(&'\"')) => {
+                    if current_key.is_some() {
+                        return Err(SomeError {
+                            msg: format!("Expected  :  after key got  \"  "),
+                        });
+                    }
+                    current_key = Some(self.parse_string()?);
+                }
+
+                (ParseState::Key, Some(&':')) => {
+                    self.pop();
+                    parse_state = ParseState::Value;
+                }
+
+                (ParseState::Value, Some(&'"')) => {
+                    let value = self.parse_string()?;
+                    let key = current_key.take().unwrap();
+                    keys.insert(key, JsonToken::String(value));
+                }
+
+                (ParseState::Value, Some(&',')) => {
+                    expect_one_more_kv_pair = true;
+                    self.pop();
+                }
+                (_, Some(unexpected)) => {
+                    return Err(SomeError {
+                        msg: format!("Unexpected token {}", unexpected),
+                    })
+                }
+                (_, None) => unreachable!(),
+            }
         }
 
+        if expect_one_more_kv_pair {
+            return Err(SomeError {
+                msg: "Expected one more key-value pair after comma".to_string(),
+            });
+        }
+
+        self.pop();
+
         return Ok(JsonObject {
-            keys: HashMap::new(),
+            keys: if keys.len() > 0 { Some(keys) } else { None },
         });
     }
 
     fn parse_string(&mut self) -> Result<String, SomeError> {
-        todo!()
+        let mut s = String::new();
+
+        if Some(&'\"') != self.pop() {
+            return Err(SomeError {
+                msg: "Expected string to start with  \"  ".to_string(),
+            });
+        }
+
+        while let Some(c) = self.pop() {
+            if c == &'"' {
+                self.pop();
+                break;
+            }
+            s.push(*c);
+        }
+
+        if self.is_done() {
+            return Err(SomeError {
+                msg: "Unterminated string".to_string(),
+            });
+        }
+
+        return Ok(s);
     }
 
     fn peek(&self) -> Option<&char> {
@@ -53,12 +142,29 @@ impl Parser {
     }
 
     fn pop(&mut self) -> Option<&char> {
+        let val = self.chars.get(self.index);
         self.index += 1;
-        self.chars.get(self.index)
+        val
+    }
+
+    fn skip_whitespace(&mut self) -> bool {
+        while !self.is_done() && (self.peek() == Some(&' ') || self.peek() == Some(&'\n')) {
+            self.pop();
+        }
+        // Some error occured
+        if self.is_done() {
+            return false;
+        }
+        true
     }
 }
 
 fn parse_json(input: &str) -> Result<JsonObject, SomeError> {
+    if input == "" {
+        return Err(SomeError {
+            msg: "No json object".to_string(),
+        });
+    }
     return Parser::new(input).parse_json();
 }
 
@@ -80,6 +186,51 @@ mod tests {
     #[test]
     fn empty_object() {
         let result = parse_json("{}");
-        assert!(result.is_ok())
+        assert!(result.unwrap() == JsonObject { keys: None })
+    }
+
+    #[test]
+    fn kv_after_comma() {
+        let result = parse_json(r#"{"key": "value",}"#);
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn non_double_quoted_property() {
+        let result = parse_json(
+            r#"{
+  "key": "value",
+  key2: "value"
+}"#,
+        );
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn simple_key_value() {
+        let result = parse_json(r#"{"key": "value"}"#).unwrap();
+        let keys: HashMap<String, JsonToken> =
+            HashMap::from([("key".to_string(), JsonToken::String("value".to_string()))]);
+
+        let compare = JsonObject { keys: Some(keys) };
+
+        assert_eq!(result, compare)
+    }
+
+    #[test]
+    fn simple_key_value_with_comma() {
+        let result = parse_json(
+            r#"{
+  "key": "value",
+  "key2": "value"
+}"#,
+        )
+        .unwrap();
+        let keys: HashMap<String, JsonToken> =
+            HashMap::from([("key".to_string(), JsonToken::String("value".to_string())), ("key2".to_string(), JsonToken::String("value".to_string()))]);
+
+        let compare = JsonObject { keys: Some(keys) };
+
+        assert_eq!(result, compare)
     }
 }
